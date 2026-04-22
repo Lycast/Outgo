80f75b4149b1e1e75b9ed8d311968b525135a091
anthony <anthonybrenon49@gmail.com>
2026-04-22 13:28:06 +0200

    add Url

---
+++ android/core/src/main/kotlin/fr/abknative/outgo/android/core/Url.kt
@@ -0,0 +1,8 @@
+package fr.abknative.outgo.android.core
+
+object Url {
+    const val FORM_URL = "https://docs.google.com/forms/d/e/1FAIpQLSfxRBuUn4epj2eL4qNXZTpLfqNlP9qmPGZCNrtlh2GsJB3zAQ/viewform?usp=dialog"
+    const val CONTACT_URL = "https://abknative.fr/contact"
+    const val OUTGO_URL = "https://abknative.fr/outgo"
+    const val STORE_URL = "https://play.google.com/store/apps/details?id=fr.abknative.outgo"
+}
