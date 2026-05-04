528a290b84807a80107d4afa9c73c0ebe00a1e58
anthony <anthonybrenon49@gmail.com>
2026-05-04 20:22:30 +0200

    Bump version to 1.0.6 for release

--- android/app/build.gradle.kts
+++ android/app/build.gradle.kts
@@ -10,8 +10,8 @@ android {
 
     defaultConfig {
         applicationId = "fr.abknative.outgo"
-        versionCode = 6
-        versionName = "1.0.5"
+        versionCode = 7
+        versionName = "1.0.6"
     }
 
     packaging {
